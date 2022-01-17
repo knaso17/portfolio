8931569bc9dd0fcf0585252e2d38f175c5ccdfbc
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-17 08:40:15 -0600

    added logo and updated text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,21 @@ export default function IndexPage () {
   return (
     <React.Fragment>
       <header>
-
+        <StaticImage
+            src="../images/KNLogo.png"
+            alt="KN logo"
+            placeholder="blurred"
+            layout="fixed"
+            style={{
+              width: "250px",
+              height: "250px",
+              borderRadius: "125px",
+            }}
+          />
       </header>
       <main>
         <title>Krista Naso's Portfolio</title>
-        <h1>Welcome!</h1>
+        <h1>Krista Naso's Portfolio</h1>
         <p>More content coming soon!</p>
       </main>
     </React.Fragment>
