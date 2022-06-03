ed909627087b05e5b4d3da68d174d9eb7af9576a
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-03 12:55:24 -0500

    slider test

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -6,14 +6,14 @@ import sliderButton from "./sliderButton"
 import { sliderData } from "./sliderData";
 
 export default function Slider () {
-  let [slideIndex, setSlideIndex] = useState(1);
+  let [slideIndex, setSlideIndex] = useState(0);
 
   const nextSlide = () => {
-    slideIndex !== sliderData.length ? setSlideIndex(slideIndex++) : setSlideIndex(1);
+    slideIndex !== sliderData.length - 1 ? setSlideIndex(slideIndex++) : setSlideIndex(0);
   }
 
   const prevSlide = () => {
-    slideIndex !== 1 ? setSlideIndex(slideIndex--) : setSlideIndex(sliderData.length);
+    slideIndex !== 0 ? setSlideIndex(slideIndex--) : setSlideIndex(sliderData.length - 1);
   }
 
   const moveDot = index => {
@@ -22,23 +22,12 @@ export default function Slider () {
 
   return (
     <div className="container-slider">
-            {sliderData.map((obj, index) => {
-                return (
-                    <div
-                    key={obj.id}
-                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-                    >
-                        <img
-                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
-                        />
-                    </div>
-                )
-            })}
+            <h2>{sliderData[slideIndex]}</h2>
             <sliderButton moveSlide={nextSlide} direction={"next"} />
             <sliderButton moveSlide={prevSlide} direction={"prev"}/>
 
             <div className="container-dots">
-                {Array.from({length: 5}).map((item, index) => (
+                {Array.from({length: 3}).map((item, index) => (
                     <div
                     onClick={() => moveDot(index + 1)}
                     className={slideIndex === index + 1 ? "dot active" : "dot"}
