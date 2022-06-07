e4e9be67d1a342e3904ca55a4a7eb0dd0d71c12c
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-07 09:53:19 -0500

    updated container and project styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -21,7 +21,7 @@ export default function Projects() {
               }}
             />
           </section>
-          <div>
+          <div className={style.projectText}>
             <h3>Leashed</h3>
             <p>
               Leashed is Tinder for dogs! A location-based mobile app that lets you create a profile for your fluffy friend to meet local dogs and set up playdates. The swipe right functionality lets you like dogs in your area to find matches and create lasting friendships! Built by a team of four engineers who used a Firebase backend with authorization and Firestore as the noSQL database. This app was designed with an appealing user interface that utilized React Native, Paper, and Expo framework. The team organized the development process by taking advantage of a Kanban project management system. Team Members: Soli Choi, Krista Naso, Ashley Tran and Tiffany McNerlin.
@@ -41,7 +41,7 @@ export default function Projects() {
               }}
             />
           </section>
-          <div>
+          <div className={style.projectText}>
             <h3>Honeflower Movies</h3>
             <p>
               Billions upon billions radio telescope with pretty stories for which there's little good evidence a mote of dust suspended in a sunbeam prime number the ash of stellar alchemy. Are creatures of the cosmos laws of physics paroxysm of global death hydrogen atoms invent the universe extraplanetary. Vastness is bearable only through love bits of moving fluff a very small stage in a vast cosmic arena emerged into consciousness citizens of distant epochs descended from astronomers and billions upon billions upon billions upon billions upon billions upon billions upon billions.
