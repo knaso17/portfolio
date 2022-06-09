c821f4086874e8876418097282860b42af0576d0
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-08 22:05:04 -0500

    updated recs component styling

--- src/components/recommendations.js
+++ src/components/recommendations.js
@@ -16,6 +16,7 @@ export default function Recommendations() {
               {`  Ashley worked with Krista on the same team`}
             </p>
           </div>
+          <hr />
           <div>
             <p className={style.recContent}>
               "Krista will be an essential member of any team she works on. When we worked together on our project, \"Leashed,\" she always gave a helping hand whenever she could, and never shied away from a tedious debugging session. Even in times of stress, Krista seemed to keep a sound mind and presence. She contributed even more to the team by 1) coding our user authentication process via Firebase, 2) attaching integral data to our global state so that the team could utilize it via Firebase and React Context, and 3) creating the schema for our project's design plan. She was also great at doing her own research and bringing forth technologies that benefitted the project such as React Native Paper. Krista always has something to show for her work, and her organization and time management skills are top-notch."
@@ -25,6 +26,7 @@ export default function Recommendations() {
               {`  Tiffany worked with Krista on the same team`}
             </p>
           </div>
+          <hr />
           <div>
             <p className={style.recContent}>
               "Working with Krista on our first group project for the Grace Hopper Program was an amazing experience. Krista is an outstanding software engineer whose determination to master new technology was not only admirable, but also motivated those around her to dig deeper into learning the logic behind our work. Krista's top priority was understanding and implementing authentication for our application. She took the initiative to thoroughly research JSON web tokens and password hashing on behalf of the team. She got our team up to speed on the steps needed to ensure our site was secure, and during code review, she asked concise, clarifying questions that helped make our code stronger. I would be delighted to have the opportunity to work with Krista again in the future, and I have no doubt that Krista will continue to thrive with whatever company is lucky enough to have her on their team."
