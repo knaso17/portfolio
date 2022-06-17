a4115d65cea09fa3a0fffb1c8cbe9553bd480e38
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-17 12:50:25 -0500

    addssr file to attempt lighthouse fixes

---
+++ gatsby-ssr.js
@@ -0,0 +1,16 @@
+
+const HtmlAttributes = {
+  lang: "en"
+}
+
+const HeadComponents = [
+  <meta name="description" content="The personal portfolio of Krista Naso a Front End Developer from the Chicago Area. View projects, recommendations, and proficient technologies!"/>
+]
+
+exports.onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+}) => {
+  setHtmlAttributes(HtmlAttributes)
+  setHeadComponents(HeadComponents)
+}
